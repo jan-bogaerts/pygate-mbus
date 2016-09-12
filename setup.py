418663696f053530843f514e750d46f478004d45
Jan Bogaerts <jb@allthingstalk.com>
2016-09-12 20:04:36 +0200

    init checkin

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pygate_mbus',
+    version='1.0',
+    packages=['pygate_mbus'],
+    url='www.allthingstalk.com',
+    license='',
+    author='Jan Bogaerts',
+    author_email='jb@allthingstalk.com',
+    description="plugin for the pygate: connect devices that communicate with the mbus protocol"
+)
